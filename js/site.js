0e51be499a18c6f7422e2a36502e44b501d59978
CassandraK <ko.cassie@gmail.com>
2023-11-07 20:30:33 -0500

    feat: added notes for local hosting setup

--- js/site.js
+++ js/site.js
@@ -1,33 +1,27 @@
-import * as THREE from 'three';
+import * as THREE from 'three'; //only works thru vite... npx vite
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-// Scene
 var scene = new THREE.Scene();
 
-// Camera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5; // Make sure the camera is not too close or too far
+camera.position.z = 5; 
 
-// Renderer
 var renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 0); // Set clear color to black with full transparency
+renderer.setClearColor(0x000000, 0); 
 document.body.appendChild(renderer.domElement);
 
-// OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Lighting
-var ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
 scene.add(ambientLight);
 
-// GLTFLoader
 const loader = new GLTFLoader();
 loader.load(
-    'assets/scene.gltf', // Make sure this path is correct
+    'assets/scene.gltf', 
     function (gltf) {
-        console.log('Model loaded:', gltf); // Confirm the model loads
+        console.log('Model loaded:', gltf); 
         scene.add(gltf.scene);
         animate();
     },
@@ -37,22 +31,19 @@ loader.load(
     }
 );
 
-// Add a simple geometry to test basic rendering
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// Animation loop
 function animate() {
     requestAnimationFrame(animate);
     controls.update(); // Only required if controls.enableDamping or controls.autoRotate are set to true
     renderer.render(scene, camera);
-    cube.rotation.x += 0.01; // Rotate the cube to see if the animation loop is working
+    cube.rotation.x += 0.01; 
     cube.rotation.y += 0.01;
 }
 
-// Handle window resize
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -61,4 +52,4 @@ function onWindowResize() {
 
 window.addEventListener('resize', onWindowResize, false);
 
-animate(); // Start the animation loop
+animate(); 
